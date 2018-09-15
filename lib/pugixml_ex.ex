cc6b87552fb060b4b7f1eaf7b585d822e1116a52
Valentin Zberea <valentin.zberea@gmail.com>
2018-09-15 16:16:48 +0200

    Initial commit

---
+++ lib/pugixml_ex.ex
@@ -0,0 +1,18 @@
+defmodule PugixmlEx do
+  @moduledoc """
+  Documentation for PugixmlEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PugixmlEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pugixml_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule PugixmlExTest do
+  use ExUnit.Case
+  doctest PugixmlEx
+
+  test "greets the world" do
+    assert PugixmlEx.hello() == :world
+  end
+end
